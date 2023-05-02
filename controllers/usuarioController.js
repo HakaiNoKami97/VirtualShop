df690c6491073069fbbf55b13c76c3a9e311b72e
HakaiNoKami97 <nokamihakai@gmail.com>
2023-05-02 14:08:39 -0500

    Cambiar estado de Colaborador

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -36,18 +36,22 @@ const login_usuario = async function(req,res){
    
     if(usuarios.length >= 1){
         //CORREO EXISTE
-        bcrypt.compare(data.password, usuarios[0].password, async function(err,check){
-            if(check){
-                //
-                res.status(200).send({
-                    token:jwt.createToken(usuarios[0]),
-                    usuario: usuarios[0]
-                });
-            }else{
-                res.status(200).send({data:undefined,message: 'La contraseña es incorrecta.'});
-            }
-            
-        });
+        if(usuarios[0].estado){
+            bcrypt.compare(data.password, usuarios[0].password, async function(err,check){
+                if(check){
+                    //
+                    res.status(200).send({
+                        token:jwt.createToken(usuarios[0]),
+                        usuario: usuarios[0]
+                    });
+                }else{
+                    res.status(200).send({data:undefined,message: 'La contraseña es incorrecta.'});
+                }
+                
+            });
+        }else{
+            res.status(200).send({data:undefined,message: 'Su cuenta esta desactivada.'});   
+        }
     }else{
         res.status(200).send({data:undefined,message: 'No se encontró el correo electrónico.'});
     }
@@ -110,10 +114,36 @@ const actualizar_usuario_admin = async function(req,res){
     }
 }
 
+const cambiar_estado_usuario_admin = async function(req,res){
+    if(req.user){
+
+        let id = req.params['id'];
+        let data = req.body;
+
+        let nuevo_estado = false;
+
+        if(data.estado){
+            nuevo_estado = false;
+        }else{
+            nuevo_estado = true;
+        }
+
+        let usuario = await Usuario.findByIdAndUpdate({_id:id},{
+            estado: nuevo_estado
+        });
+
+        res.status(200).send(usuario);
+
+    }else{
+        res.status(500).send({data:undefined,message: 'ErrorToken'});
+    }
+}
+
 module.exports = {
     registro_usuario_admin,
     login_usuario,
     listar_usuario_admin,
     obtener_usuario_admin,
-    actualizar_usuario_admin
+    actualizar_usuario_admin,
+    cambiar_estado_usuario_admin
 }
