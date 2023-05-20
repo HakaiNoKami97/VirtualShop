a7efe1dd160b9d68c1907526aa2e1af40dc276c6
HakaiNoKami97 <nokamihakai@gmail.com>
2023-05-20 12:52:40 -0500

    Login de cliente y generar token de autenticacion

--- controllers/clienteController.js
+++ controllers/clienteController.js
@@ -1,5 +1,6 @@
 var Cliente = require('../models/cliente');
 var bcrypt = require('bcrypt-nodejs');
+var jwt = require('../helpers/cjwt');
 
 const registro_cliente_ecommerce = async function(req,res){
     let data = req.body;
@@ -24,6 +25,35 @@ const registro_cliente_ecommerce = async function(req,res){
     }
 }
 
+const login_cliente = async function(req,res){
+    var data = req.body;
+
+    var clientes = await Cliente.find({email: data.email});
+   
+    if(clientes.length >= 1){
+        //CORREO EXISTE
+        if(clientes[0].estado){
+            bcrypt.compare(data.password, clientes[0].password, async function(err,check){
+                if(check){
+                    //
+                    res.status(200).send({
+                        token:jwt.createToken(clientes[0]),
+                        cliente: clientes[0]
+                    });
+                }else{
+                    res.status(200).send({data:undefined,message: 'La contraseña es incorrecta.'});
+                }
+                
+            });
+        }else{
+            res.status(200).send({data:undefined,message: 'Su cuenta esta desactivada.'});   
+        }
+    }else{
+        res.status(200).send({data:undefined,message: 'No se encontró el correo electrónico.'});
+    }
+}
+
 module.exports = {
-    registro_cliente_ecommerce
+    registro_cliente_ecommerce,
+    login_cliente
 }
