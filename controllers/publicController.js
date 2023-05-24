39a94f9797880d7bdb43a3215eec5d4da8a6858c
HakaiNoKami97 <nokamihakai@gmail.com>
2023-05-24 00:19:28 -0500

    Mostrar todos los productos en la tienda

--- controllers/publicController.js
+++ controllers/publicController.js
@@ -1,19 +1,20 @@
 var Producto = require('../models/producto');
 
 const obtener_nuevos_productos = async function(req,res){
-
     var productos = await Producto.find({estado:true}).sort({createdAt:-1}).limit(4);
     res.status(200).send(productos);
-
 }
 const obtener_productos_recomendados = async function(req,res){
-
     var productos = await Producto.find({estado:true}).limit(8);
     res.status(200).send(productos);
-
+}
+const obtener_productos_shop = async function(req,res){
+    var productos = await Producto.find({estado:true});
+    res.status(200).send(productos);
 }
 
 module.exports = {
     obtener_nuevos_productos,
-    obtener_productos_recomendados
+    obtener_productos_recomendados,
+    obtener_productos_shop
 }
