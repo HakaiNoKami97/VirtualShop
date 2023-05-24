7e424d35d8e48c1aed4d87359a1b49b508c926e8
HakaiNoKami97 <nokamihakai@gmail.com>
2023-05-24 00:59:12 -0500

    Paginacion para productos

--- controllers/publicController.js
+++ controllers/publicController.js
@@ -9,7 +9,7 @@ const obtener_productos_recomendados = async function(req,res){
     res.status(200).send(productos);
 }
 const obtener_productos_shop = async function(req,res){
-    var productos = await Producto.find({estado:true});
+    var productos = await Producto.find({estado:true}).sort({createdAt:-1});
     res.status(200).send(productos);
 }
 
