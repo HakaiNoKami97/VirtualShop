de781c3ca1d5deab0b963eec0372b0b974f277ec
HakaiNoKami97 <nokamihakai@gmail.com>
2023-05-25 17:49:13 -0500

    Implementacion de sockets

--- app.js
+++ app.js
@@ -2,41 +2,57 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyparser = require('body-parser');
 var port = process.env.port || 4201;
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
 var app = express();
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: { origin: '*'}
+});
+
+io.on("connection", (socket) => {
+  // ...
+  socket.on('emit_method',function(data){
+    io.emit('semit_method',data);
+    console.log(data);
+  });
+});
+
+
 var cliente_router = require('./routes/cliente');
 var usuario_router = require('./routes/usuario');
 var producto_router = require('./routes/producto');
 var public_router = require('./routes/public');
 var customer_router = require('./routes/customer');
+
 app.use(bodyparser.urlencoded({limit: '50mb', extended: true}));
 app.use(bodyparser.json({limit: '50mb', extended: true}));
-/*const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://nokamihakai:Eygk8fFgiTvtH2kd@cluster0.7jl1air.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});*/
-mongoose.connect('mongodb://127.0.0.1:27017/tienda', (err, res)=>{
+
+mongoose.connect('mongodb://127.0.0.1:27017/tienda',(err,res)=>{
     if(err){
         console.log(err);
     }else{
-        app.listen(port,function(){
+        httpServer.listen(port,function(){
             console.log('Servidor corriendo '+port);
         });
     }
 });
-app.use((req, res,next)=>{
-    res.header('Access-Control-Allow-Origin','*');
+
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*'); 
     res.header('Access-Control-Allow-Headers','Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods','GET, PUT, POST, DELETE, OPTIONS');
     res.header('Allow','GET, PUT, POST, DELETE, OPTIONS');
     next();
 });
+
 app.use('/api',cliente_router);
+app.use('/api',usuario_router);
 app.use('/api',usuario_router);
 app.use('/api',producto_router);
 app.use('/api',public_router);
 app.use('/api',customer_router);
+
 module.exports = app;
