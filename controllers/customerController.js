5cd25ef9734774b2f59b527553615bd2c3feb01a
HakaiNoKami97 <nokamihakai@gmail.com>
2023-05-25 19:36:53 -0500

    Eliminar producto del carrito

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -27,13 +27,28 @@ const crear_producto_carrito = async function(req,res){
 const obtener_carrito_cliente = async function(req,res){
     if(req.user){
        let carrito = await Carrito.find({cliente:req.user.sub}).populate('producto').populate('variedad').sort({createdAt:-1}).limit(5);
-       res.status(200).send(carrito);
+       let carrito_general = await Carrito.find({cliente:req.user.sub}).populate('producto').populate('variedad').sort({createdAt:-1});
+       res.status(200).send({carrito,carrito_general});
     }else{
         res.status(500).send({data:undefined,message: 'ErrorToken'});
     }
 }
 
+
+
+const eliminar_producto_carrito = async function(req,res){
+    if(req.user){
+       let id = req.params['id'];
+       let reg = await Carrito.findByIdAndRemove({_id:id});
+       res.status(200).send(reg);
+    }else{
+        res.status(500).send({data:undefined,message: 'ErrorToken'});
+    }
+}
+
+
 module.exports = {
     crear_producto_carrito,
-    obtener_carrito_cliente
+    obtener_carrito_cliente,
+    eliminar_producto_carrito
 }
--- routes/customer.js
+++ routes/customer.js
@@ -4,4 +4,5 @@ var cauthenticate = require('../middlewares/cauthenticate');
 var api = express.Router();
 api.post('/crear_producto_carrito', cauthenticate.decodeToken, customerController.crear_producto_carrito);
 api.get('/obtener_carrito_cliente', cauthenticate.decodeToken, customerController.obtener_carrito_cliente);
+api.delete('/eliminar_producto_carrito/:id', cauthenticate.decodeToken, customerController.eliminar_producto_carrito);
 module.exports = api;
