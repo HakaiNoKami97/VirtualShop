ec1ae6dfe72ec8c76856d58020576f73ac86f55d
HakaiNoKami97 <nokamihakai@gmail.com>
2023-05-28 15:27:03 -0500

    Detalle de venta

--- routes/venta.js
+++ routes/venta.js
@@ -3,4 +3,5 @@ var ventasController = require('../controllers/ventasController');
 var authenticate = require('../middlewares/authenticate');
 var api = express.Router();
 api.get('/obtener_ventas_admin/:inicio/:hasta', authenticate.decodeToken, ventasController.obtener_ventas_admin);
+api.get('/obtener_detalles_venta_admin/:id', authenticate.decodeToken, ventasController.obtener_detalles_venta_admin);
 module.exports = api;
